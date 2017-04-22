a26061fc78446965a35215b69971f28f1697a99b
williambl <william@williambl.com>
2017-04-22 13:54:58 +0100

    Made an orbiting moon

---
+++ Assets/Scripts/ForceApplier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceApplier : MonoBehaviour {
+
+	public Vector3 force;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().AddForce (force);
+	}
+}
