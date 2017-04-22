c7b15d1f9a7a07ac0c63ec5b8cc566a42cc71a11
williambl <william@williambl.com>
2017-04-22 10:27:47 +0100

    Sort of got movement working

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float rotateSpeed = 1;
+	public float moveSpeed = 1;
+
+	private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float x = Input.GetAxis ("Vertical") * rotateSpeed;
+		float y = Input.GetAxis ("Horizontal") * moveSpeed;
+
+		rigid.AddRelativeForce (new Vector3 (x, 0, 0));
+		transform.localRotation *= Quaternion.Euler (new Vector3 (0, y, 0));
+	}
+}
