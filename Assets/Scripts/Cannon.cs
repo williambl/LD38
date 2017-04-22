dff08361cdb06fa6c26ce29c3674081cd7b82ecf
williambl <william@williambl.com>
2017-04-22 14:49:57 +0100

    Added a cannon
    It launches you to another planet

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+	public Vector3 force;
+
+	void OnTriggerEnter (Collider other)
+	{
+		other.GetComponent<Rigidbody> ().AddRelativeForce (force);
+	}
+}
