0441ce19db3dba4a45cd0125306e925e3fa0b2f9
williambl <william@williambl.com>
2017-04-22 21:12:33 +0100

    Added coin script

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	public PlayerScore playerScore;
+
+	void Start ()
+	{
+		playerScore = GameObject.Find ("Player").GetComponent<PlayerScore> ();
+	}
+
+	void OnTriggerEnter ()
+	{
+		playerScore.score++;
+		Destroy (this.gameObject);
+	}
+}
