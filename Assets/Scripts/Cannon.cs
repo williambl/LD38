b4ddd8c026edd8e3ba6d27384d3d42db6d512f8e
williambl <william@williambl.com>
2017-04-22 21:43:08 +0100

    Changed cannons and a new planet

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour {
 
-	public Vector3 force;
+	public float force;
 
 	void OnTriggerEnter (Collider other)
 	{
-		other.GetComponent<Rigidbody> ().AddRelativeForce (force);
+		if (other.gameObject.tag != "Player") {
+			return;
+		}
+
+		other.transform.position = transform.position;
+
+		Rigidbody rigid = other.GetComponent<Rigidbody> ();
+		rigid.velocity = Vector3.zero;
+		rigid.angularVelocity = Vector3.zero;
+		rigid.AddForce (transform.up * force);
+		Debug.Log (transform.up * force);
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.tag != "Player") {
+			return;
+		}
+		other.transform.localPosition += transform.up.normalized * 2;
+		other.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		other.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
 	}
 }
