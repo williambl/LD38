a7523c39f6fa28cb7fea1b20bfbedd8fcb7a8829
williambl <william@williambl.com>
2017-04-23 16:20:48 +0100

    Added UI
    Super Mario 64-style health bar, and a rather boring points thing.

---
+++ Assets/Scripts/Player/UIControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour {
+
+	public PlayerScore playerScore;
+
+	public Image health;
+	public Text score;
+
+	// Use this for initialization
+	void Start () {
+		playerScore = GetComponent<PlayerScore> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		health.fillAmount = playerScore.health / 8f;
+		score.text = "Score: " + playerScore.score;
+	}
+}
